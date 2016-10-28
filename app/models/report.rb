0174aff4fbd38a4f263fdb4f8bc799d225839fde
Tomasz Cieńciała <tomasz.cienciala@akra.net>
2016-10-28 09:59:03 +0200

    Report Model Presence Validations

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,5 @@
 class Report < ApplicationRecord
+  validates_presence_of :category
+  validates_presence_of :email
+  validates_presence_of :description
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Report, type: :model do
 
+  describe 'When saving Report' do
+    it 'is not valid without description' do
+      report = Report.new(description: nil, email: Faker::Internet.email, category: SetCategories.new.call.sample)
+      expect(report).to_not be_valid
+    end
+    it 'is not valid without email' do
+      report = Report.new(description: Faker::Lorem.paragraph(sentence_count=8), email: nil, category: SetCategories.new.call.sample)
+      expect(report).to_not be_valid
+    end
+    it 'is not valid without category' do
+      report = Report.new(description: Faker::Lorem.paragraph(sentence_count=8), email: Faker::Internet.email, category: nil)
+      expect(report).to_not be_valid
+    end
+
+    it 'is valid with presence of description, email & category' do
+      report = Report.new(description: Faker::Lorem.paragraph(sentence_count=8), email: Faker::Internet.email, category: SetCategories.new.call.sample)
+      expect(report).to be_valid
+    end
+
+  end
 end
