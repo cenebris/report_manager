bc1901598f67cf8c336eb1d859a3e9a3e5a5f05f
Tomasz Cieńciała <tomasz.cienciala@akra.net>
2016-10-28 13:10:09 +0200

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Report.destroy_all
+puts 'All Reports deleted.'
+
+a = 123.times do
+  report_category = SetCategories.new.call.sample
+  report_price = SetReportPrice.new.call report_category
+  report_completion_time = SetCompletionTime.new.call report_category
+
+  Report.create(
+      description: Faker::Lorem.paragraph(sentence_count=8),
+      email: Faker::Internet.email,
+      category: report_category,
+      read: false,
+      price: report_price,
+      completion_time: report_completion_time
+  )
+end
+puts "#{a} new Reports created"
